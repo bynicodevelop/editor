2e8bcf0d700f30362e9dba3abf6e438920654d32
nico-develop <contact@nico-develop.fr>
2020-06-11 08:59:48 +0200

    Ajout des menus de base

--- src/background.js
+++ src/background.js
@@ -1,12 +1,13 @@
 'use strict'
 /* global __static */
 
-import { app, protocol, BrowserWindow, Menu } from 'electron'
+import {app, protocol, BrowserWindow, Menu} from 'electron'
 import {
-  createProtocol,
-  /* installVueDevtools */
+    createProtocol,
+    /* installVueDevtools */
 } from 'vue-cli-plugin-electron-builder/lib'
 import path from 'path'
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -14,85 +15,113 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 let win
 
 // Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true, standard: true } }])
+protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: {secure: true, standard: true}}])
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 920, height: 600, icon: path.join(__static, 'icon.png'), webPreferences: {
-    // Use pluginOptions.nodeIntegration, leave this alone
-    // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-    nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-  }})
+const template = [
+    {
+        label: "Edit",
+        submenu: [
+            {label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:"},
+            {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:"},
+            {type: "separator"},
+            {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+            {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+            {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"},
+            {label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:"}
+        ]
+    }
+]
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    const template = [ ]
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 920, height: 600, icon: path.join(__static, 'icon.png'), webPreferences: {
+            // Use pluginOptions.nodeIntegration, leave this alone
+            // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+            nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+        }
+    })
+
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+      template.push({
+        label: "Dev tools",
+        submenu: [
+          {
+            label: "Refresh",
+            accelerator: "Shift+CmdOrCtrl+R",
+            click() {
+              win.reload();
+            }
+          }
+        ]
+      })
+
+        // Load the url of the dev server if in development mode
+        win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+        if (!process.env.IS_TEST) win.webContents.openDevTools()
+    } else {
+        createProtocol('app')
+        // Load the index.html when not in development
+        win.loadURL('app://./index.html')
+    }
 
     const menu = Menu.buildFromTemplate(template)
     Menu.setApplicationMenu(menu)
 
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
-  }
-
-  win.on('closed', () => {
-    win = null
-  })
+    win.on('closed', () => {
+        win = null
+    })
 }
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    // Devtools extensions are broken in Electron 6.0.0 and greater
-    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
-    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
-    // If you are not using Windows 10 dark mode, you may uncomment these lines
-    // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
-    // try {
-    //   await installVueDevtools()
-    // } catch (e) {
-    //   console.error('Vue Devtools failed to install:', e.toString())
-    // }
+    if (isDevelopment && !process.env.IS_TEST) {
+        // Install Vue Devtools
+        // Devtools extensions are broken in Electron 6.0.0 and greater
+        // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+        // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+        // If you are not using Windows 10 dark mode, you may uncomment these lines
+        // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
+        // try {
+        //   await installVueDevtools()
+        // } catch (e) {
+        //   console.error('Vue Devtools failed to install:', e.toString())
+        // }
 
-  }
-  createWindow()
+    }
+    createWindow()
 })
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', data => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
-    })
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
-  }
+    if (process.platform === 'win32') {
+        process.on('message', data => {
+            if (data === 'graceful-exit') {
+                app.quit()
+            }
+        })
+    } else {
+        process.on('SIGTERM', () => {
+            app.quit()
+        })
+    }
 }
